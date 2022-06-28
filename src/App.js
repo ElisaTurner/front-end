14edf0d739191bf724d73cb5f83f367ccbd81395
elisa turner <elisa.turner@podium.com>
2022-06-28 14:32:26 -0600

    updating frontend

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
+import * as React from "react";
+import { ThemeProvider } from "@podiumhq/design-system";
 
-function App() {
+function App({ children }) {
   return (
     <div className="App">
       <header className="App-header">landing page</header>
+      <ThemeProvider>{children} </ThemeProvider>
     </div>
   );
 }
